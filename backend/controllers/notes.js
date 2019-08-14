36e061f4baf0a1edf6bce5beffb86f11693b54d3
Jorge Teixeira <jorge92@mailbox.org>
2019-08-14 15:17:40 +0100

    added express route to get notes by id (closes #8)

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -8,6 +8,19 @@ notesRouter.get('/', async (req, res) => {
   res.json(notes.map((note) => note.toJSON()));
 });
 
+// Get Note By Id
+notesRouter.get('/:id', async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).json({ error: 'Note not found' });
+    }
+    return res.send(note);
+  } catch (e) {
+    return next(e);
+  }
+});
+
 // Create Note
 notesRouter.post('/', async (req, res, next) => {
   try {
@@ -23,10 +36,27 @@ notesRouter.post('/', async (req, res, next) => {
   }
 });
 
+// Update Note By Id
+notesRouter.patch('/:id', async (req, res, next) => {
+  try {
+    const { nModified } = await Note
+      .updateOne({ _id: req.params.id }, req.body, { new: true });
+    if (nModified === 0) {
+      return res.status(400).json({ nModified });
+    }
+    return res.json({ nModified });
+  } catch (e) {
+    next(e);
+  }
+});
+
 // Delete Note By Id
 notesRouter.delete('/:id', async (req, res, next) => {
   try {
-    await Note.deleteOne({ _id: req.params.id });
+    const { deletedCount } = await Note.deleteOne({ _id: req.params.id });
+    if (deletedCount === 0) {
+      return res.status(404).end();
+    }
     return res.status(204).end();
   } catch (e) {
     return next(e);
--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -4,10 +4,10 @@ const unknownEndpoint = (req, res) => {
 
 const errorHandler = (error, req, res, next) => {
   if (error.name === 'ValidationError') {
-    return res.status(400).send({ error: error.message });
+    return res.status(400).send({ errName: error.name, errMsg: error.message });
   }
   if (error.name === 'CastError') {
-    return res.status(400).send({ error: error.message });
+    return res.status(400).send({ errName: error.name, errMsg: error.message });
   }
 
   return next(error);
