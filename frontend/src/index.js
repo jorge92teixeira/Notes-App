d75e8e6139a460f92432c84d32ce17447c002eb8
Jorge Teixeira <jorge92@mailbox.org>
2019-08-28 22:21:42 +0100

    created redux visibilityFilter reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import notesAppReducer from './reducers/notesAppReducer';
 
-import App from './components/app';
+import App from './components/App';
 
-ReactDom.render(<App />, document.getElementById('root'));
+const store = createStore(notesAppReducer);
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
